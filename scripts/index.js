0de9d038793a0be443d64cec2ad4f7a05f42b605
divneofficial <89173504+divneofficial@users.noreply.github.com>
2021-08-18 22:50:20 -0400

    uploaded files

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".nav-switch");
+    var $navLink1 = $(".nav-link-1");
+    var $navLink2 = $(".nav-link-2");
+    var $navLink3 = $(".nav-link-3");
+
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $navLink1.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $navLink2.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $navLink3.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+});
+});
